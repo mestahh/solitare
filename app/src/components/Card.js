4bff117320b2c80401a10d85ad34b6f25af441ae
Balázs Mester <balazs.mester@getbridge.com>
2023-01-23 09:14:49 +0100

    Basic drop

--- app/src/components/Card.js
+++ app/src/components/Card.js
@@ -6,12 +6,38 @@ const Card = props => {
     const shape = props.shape.toLowerCase();
     const index = props.index;
     const reversedStyle = props.reversed ? 'card-wrapper-inner reversed' : 'card-wrapper-inner';
-
+    const id = shape + '_' + value;
 
     const imageUrl = '/cards/' + value + '_of_' + shape + 's.png';
 
+    const dragStartHandler = (event) => {
+        event.dataTransfer.setData("card", id);
+    }
+
+    const allowDrop = (ev) => {
+        if (props.reversed) {
+            return false;
+        }
+        // here I have to examine wether I can drop the card here or not
+        ev.preventDefault();
+    }
+
+    const drop = (ev) => {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("card");
+        document.getElementById(id).appendChild(document.getElementById(data));
+
+        // here I have to update the column in the state with the new card
+    }
+
     return (
-        <div className="card-wrapper" style={{ top: (-8 * index) + 'rem' }}>
+        <div
+            id={shape + '_' + value}
+            className="card-wrapper"
+            onDragStart={dragStartHandler}
+            onDragOver={allowDrop}
+            onDrop={drop}
+            style={{ top: (-8 * index) + 'rem' }}>
             <div className={reversedStyle}>
                 <div className="card-front">
                     <img src={imageUrl} alt="Avatar" />
--- app/src/components/Tableau.js
+++ app/src/components/Tableau.js
@@ -4,25 +4,25 @@ import Card from './Card';
 
 function Tableau(props) {
 
-    return (
-      <section id="tableau">
-        { 
-          props.tableau.map((column) => (
-            <div className="tableau-column">
-              {
-                column.column.map((card, index) => (
-                  <Card 
-                  value={card.value} 
-                  shape={card.shape} 
+  return (
+    <section id="tableau">
+      {
+        props.tableau.map((column, index) => (
+          <div className="tableau-column" key={index}>
+            {
+              column.column.map((card, index) => (
+                <Card
+                  value={card.value}
+                  shape={card.shape}
                   reversed={card.reversed}
-                  index={index} 
-                  key={card.value + '_' + card.shape}/>
-                ))}
-            </div>
-          ))
-        }
-      </section>
-    );
-  }
+                  index={index}
+                  key={card.value + '_' + card.shape} />
+              ))}
+          </div>
+        ))
+      }
+    </section>
+  );
+}
 
-  export default Tableau
+export default Tableau
