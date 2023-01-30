1630151a5e126a5df402dc4da0f1c699debf40ec
Balázs Mester <balazs.mester@getbridge.com>
2023-01-30 18:38:58 +0100

    Add deck css module

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,8 +1,8 @@
-import { useEffect, useReducer, useState } from 'react';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { startDrag } from '../solitareSlice';
 import Card from './Card';
-import './Deck.css';
+import styles from './Deck.module.css';
 import EmptyCard from './EmptyCard';
 
 const Deck = (props) => {
@@ -26,8 +26,8 @@ const Deck = (props) => {
         setOpenedCardIndexes((previous) => {
             if (previous.start == 0 && previous.end == 0) {
                 return { start: 0, end: 1 };
-            } else if(previous.start + 3 == previous.end) {
-                return {start: previous.start + 1, end: previous.end + 1};
+            } else if (previous.start + 3 == previous.end) {
+                return { start: previous.start + 1, end: previous.end + 1 };
             } else {
                 return { start: previous.start, end: previous.end + 1 };
             }
@@ -39,25 +39,26 @@ const Deck = (props) => {
     }
 
     return (
-        <div className="deck">
+        <div className={styles.deck}>
             <div onClick={restorePack}><EmptyCard onDragOver={noDrop} onDrop={noDrop} /></div>
-            <div className="deck-cards">
-                <h1>{openedCardIndexes.start} {openedCardIndexes.end} </h1>
-                <div className="opened-cards">
+            <div className={styles.deckCards}>
+                <div className={styles.openedCards}>
                     {
                         props.deck.cards.slice(openedCardIndexes.start, openedCardIndexes.end).map((card, index) =>
-                            <Card
-                                onDragStart={(event) => dragStartHandler(event, card)}
-                                onDragOver={noDrop}
-                                onDrop={noDrop}
-                                card={card}
-                                index={index}
-                                cardOverlap={'-175'}
-                                key={card.id} />
+                            <div class={styles.openedCard}>
+                                <Card
+                                    onDragStart={(event) => dragStartHandler(event, card)}
+                                    onDragOver={noDrop}
+                                    onDrop={noDrop}
+                                    card={card}
+                                    index={index}
+                                    key={card.id} />
+                            </div>
                         )
                     }
+
                 </div>
-                <div className="pack" onClick={toOpenCards}>
+                <div className={styles.pack} onClick={toOpenCards}>
                     {
                         props.deck.cards.slice(openedCardIndexes.end).reverse().map((card, index) =>
                             <Card
@@ -66,7 +67,7 @@ const Deck = (props) => {
                                 onDrop={noDrop}
                                 card={card}
                                 index={index}
-                                cardOverlap={'-175'}
+                                cardOverlap={'-173'}
                                 key={card.id}
                             />
                         )
